8a05f793094f459377ea8571c011f76dd149872f
arios10 <ariosfly3@gmail.com>
2021-09-20 22:29:05 -0700

    creating logs to console and commenting out function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,27 @@
-window.alert("This is an alert! JavaScript is running!");
+// create a variable for robot name and save it
+var playerName = window.prompt("What is your robot's name?");
+
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+
+//logging a string for the first time
+console.log("This logs a string, good for leaving yourself a message");
+
+// this will do math and log 20
+console.log(10 + 10);
+
+// log playerName
+console.log("Our robot's name is " + playerName);
+
+//creat yourName variable and log
+var name = "your name";
+console.log(name);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//commenting out a function
+// fight();
